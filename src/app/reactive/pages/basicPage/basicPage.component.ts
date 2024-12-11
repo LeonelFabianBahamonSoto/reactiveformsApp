6f52078031bb5ce677dc8221f159cff46b386ff2
smarthink <smarthink@gane.com.co>
2024-12-10 21:45:38 -0500

    BasicPage changes

--- src/app/reactive/pages/basicPage/basicPage.component.ts
+++ src/app/reactive/pages/basicPage/basicPage.component.ts
@@ -20,17 +20,30 @@ export class BasicPageComponent implements OnInit {
     constructor( private fb: FormBuilder ) {}
 
     ngOnInit(): void {
-        throw new Error("Method not implemented.");
+        this.myForm.reset({ productName: '', price: 0, inStorage: 0 });
+
+        // Suscribirse a los cambios de estado para actualizaciones inmediatas
+        this.myForm.get('productName')?.statusChanges.subscribe(() => {
+            this.myForm.get('productName')?.markAsTouched(); // Marca como tocado para forzar validación
+        });
+    }
+
+    isValidField = ({ theField = '', theError = '' }): boolean => {
+        return this.myForm.get(theField)?.errors?.[theError];
     }
 
     public myForm: FormGroup = this.fb.group({
-        productName: ['',[Validators.required, Validators.minLength(3)],[]],
-        price: [0,[Validators.required, Validators.min(1)],[]],
-        inStorage: [0,[Validators.required, Validators.min(1)],[]],
+        productName: ['',[Validators.required, Validators.minLength(3), Validators.maxLength(11)], []],
+        price: ['',[Validators.required, Validators.min(100), Validators.max(1000001)],[]],
+        inStorage: ['',[Validators.required, Validators.min(1), Validators.max(101)],[]],
     });
 
     onSaveForm = (): void => {
-        if(this.myForm.invalid) return;
-        console.info('FORM: ',this.myForm.value);
+        this.myForm.markAllAsTouched();
+        if(this.myForm.invalid){
+            return
+        };
+        console.info('DATA SENT FORM: ',this.myForm.value);
+        this.myForm.reset();
     };
 }
